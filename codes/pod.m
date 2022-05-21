d94249c18cf5d50ce12a873188395aab17e9a385
Michael Raba <michael.raba@gmail.com>
2022-05-21 16:30:35 -0400

    adding citrini pod

--- codes/pod.m
+++ codes/pod.m
@@ -1,3 +1,15 @@
 % follows 3.5-3.6 of citrini and george.
 % function takes in fluctuation with azimuthal and streamwise fft applied.
 % that 'looks' like essentially a correlation in time t direction.
+
+% import data object.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (Equation A)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% take inner product after multiply each (a,b) by ra and rb , r is radius
+% use quad.
+% use eig. The return eigvec is the alpha_n , for n eigenvalues.
+% use that alpha for eq (B).
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% (Equation B)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
