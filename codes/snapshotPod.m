99cfa1d3c3058ffc07a1985db34cb0c4dfc0b627
Michael Raba <michael.raba@gmail.com>
2022-06-08 18:07:14 -0400

    pod accepts substruct of m and k..; u of m and k

--- codes/snapshotPod.m
+++ codes/snapshotPod.m
@@ -1,16 +1,16 @@
 % Snapshot POD driver function
-function snapshotPod(m,c,currentCorrMat,currentUvec)
+function snapshotPod(currentAzimuthalMode,currentCrossSection,currentCorrMat,currentUvec)
 %function pod()
 %figure(1);
 hold on;
 [ntimesteps, rMin, rMax, ss, ncs, plotOn, azimuthalSet ,azimuthalSetSize ,printStatus ,lags, blocLength, saveDir,csSet,timeSet]=constants();
      % this needs checked   
-saveStr=[saveDir 'corrMatRavg[Case]C' num2str(ncs) 'T' num2str(ntimesteps) '[crossSec]' num2str(ncs) '.mat'];
-        qq=open(saveStr);
-        Rmat_avg=qq.corrMatRavg; % Rmat(time).cs(cs).circle(=azimuthalSetSize1:18)
+%saveStr=[saveDir 'corrMatRavg[Case]C' num2str(ncs) 'T' num2str(ntimesteps) '[crossSec]' num2str(ncs) '.mat'];
+ %       qq=open(saveStr);
+       % Rmat_avg=qq.corrMatRavg; % Rmat(time).cs(cs).circle(=azimuthalSetSize1:18)
         clear qq;
         %sprintf('%s','dbg')
-for cc=1:ncs % streamwise mode % cannot exceed 1... 
+%for cc=1:ncs % streamwise mode % cannot exceed 1... 
          % xdirPostFft needs to be correct timebloc.
 
 
@@ -25,24 +25,21 @@ tTrapz=zeros(ntimesteps,1);
 %phiVec=zeros(ss,1);
 
 
-        saveStr=[saveDir 'xdirPostFft[Case]C' num2str(ncs) 'T' num2str(ntimesteps) '[crossSec]' num2str(ncs) '[TimeBloc]' num2str(timeBloc) '.mat'];
-        uu=open(saveStr);
-        xDirPostFft=qq.xDirPostFft;
+        %saveStr=[saveDir 'xdirPostFft[Case]C' num2str(ncs) 'T' num2str(ntimesteps) '[crossSec]' num2str(ncs) '[TimeBloc]' num2str(timeBloc) '.mat'];
+        %uu=open(saveStr);
+        %xDirPostFft=qq.xDirPostFft;
 
 for podModeNumber=1:3
 for rr=1:ss
 for tt=1:ntimesteps % finding the n eigenfunctions Phi^{(n)}(r)...:
-    % fftTransformedFluctuation(18).cs(3).rad  
-    % uXfft(18).cs(3).rad
-    aa=uXfft(mm).cs(cc).rad(rr).dat(tt); % this indeed varies with t.
-    % qq(4).RadialCircle(540).azimuth  
-    % must distinguish between alpha^{n} (t)
+    % currentUvec.r(7).dat  
+    aa=currentUvec.r(rr).dat(tt); % this indeed varies with t.
     bb=ctranspose(eigVec(podModeNumber)); % this t plays the role of eigvecs \in [0,N]. Should save each separate.
     ab = aa*bb;   
     tTrapz(tt) = ab;
   end % tt
 ad= trapz(tTrapz);
-phiVec(podModeNumber).c(cc).m(mm).dat(rr) = ad/(eigVal(tt,tt)*ntimesteps); % smits.eq.2.5
+phiVec(podModeNumber).c(currentCrossSection).m(currentAzimuthalMode).dat(rr) = ad/(eigVal(tt,tt)*ntimesteps); % smits.eq.2.5
 end %rr
 
 end % podMode
@@ -50,7 +47,4 @@ hold on;
 if 2 <= mm < azimuthalSetSize
 %plot(real(phiVec));
 end % if 
-end % circle mm
-end % ncs
-
-%end % fc
+end % fc
