e75dff9ae479271869971fa1909008ce84147ee5
Michael Raba <michael.raba@gmail.com>
2022-06-23 13:05:40 -0400

    avg Rms Vec routine -> CM

--- codes/avgRmsVecCM.m
+++ codes/avgRmsVecCM.m
@@ -9,13 +9,24 @@ qq=open('/home/mi/citriniPodCode/GraphsAndData-spectralAnalysis/rmsVecCM.mat')
 sprintf('%s','working..')
 
 %qq.rmsVecCM(99).m(18).dat
-for m=1:18
+for m=2:18
 sprintf('%s%d%s' , 'Reading Data: m=',m,'.')
   avgVec=zeros(540,1);
   for c=1:18
     avgVec = avgVec + qq.rmsVecCM(c).m(m).dat ;
   end %c
   rmsVecAvg(m).dat = avgVec;
+    labelStr = ['m=' num2str(azimuthalSet(m)) '.']
+    hold on
+    if m==2
+    plot(1.1e-7*flip(avgVec) ,'b--' , 'LineWidth' , 2  ,"DisplayName", labelStr)
+    else
+            plot(1.1e-7*flip(avgVec)  ,"DisplayName", labelStr)
+    end
+    legend();
+    titleStrr=['Streamwise Reynolds Shear Stress $rms(uu)$ for Azimuthal modes $m\in[' num2str(azimuthalSet(2)) ',' num2str(azimuthalSet(azimuthalSetSize)) ']$ for Ensemle-averaged Streamwise Modes $k$, $N_t \in [0,999], N_c \in[0,99]$. N.b.(1): Real part only']
+    sgtitle(titleStrr,'FontName','capitana','FontSize',14,'interpreter','latex')
+
   end %m
 
 end %fc
